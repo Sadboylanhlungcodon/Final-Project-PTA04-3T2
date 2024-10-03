8783b4b316541e33a1b354bdbbb4f0e2736f778c
Sadboylanhlungcodon <hadulong00@gmail.com>
2024-10-03 20:33:17 +0700

    Tuần 4

--- main.py
+++ main.py
@@ -2,6 +2,37 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget # them widget tu 
 from PyQt6 import uic # them chuc nang load ui
 import sys # them thu vien dieu khien he thong
 
+#Lớp đăng kí
+class SignUp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("UI-GiaoDien/signup.ui", self)
+        self.btnSignUp.clicked.connect(self.registerAccount)
+        
+    def registerAccount(self):
+        username = self.txtEmail
+        password = self.txtPassword
+        confirmPassword = self.txtconfirmPassword
+        if username.text() == "":
+            username.setFocus()
+            return
+        if password.text() == "":
+            password.setFocus()
+            return
+        if confirmPassword.text() == "":
+            confirmPassword.setFocus()
+            return
+        if username.text().find("gmail.com") == -1:
+            username.clear()
+            username.setFocus()
+            return
+        with password.text() != confirmPassword.text():
+            confirmPassword.setFocus()
+            return
+        with open("accounts.txt", "a") as file:
+            file.write(username.text() + " " + password.text() + "\n")
+        self.close()
+
 class Login(QMainWindow):
     # ham khoi tao giao dien UI
     def __init__(self):
@@ -10,11 +41,19 @@ class Login(QMainWindow):
         uic.loadUi("Ui-GiaoDien/login.ui", self)
         self.btnLogin.clicked.connect(self.checkLogin)
     def checkLogin(self):
-        if self.txtEmail.text() == "DoMinhKhoi" and self.txtPassword.text() == "123":
+        with open("accounts.txt", "r") as file:
+            data = file.readlines()
+            accounts = []
+            for line in data:
+                line = line.replace("\n", "")
+                line = line.strip()
+                accounts.append(line)
+        if (self.txtEmail.text() + ":" + self.txtPassword.text()) in accounts:
             self.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     lg = Login()
-    lg.show()
+    Su = SignUp()
+    Su.show()
     app.exec()
